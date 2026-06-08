c82660aa879eb9dfe3d43ae3024137ee6085a556
Rumaisa10 <sultanarumaisa12@gmail.com>
2026-06-08 18:46:29 +0100

    testing

--- lib/claude.ts
+++ lib/claude.ts
@@ -1,6 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
-if (!process.env.GEMINI_API_KEY) throw new Error('Missing GEMINI_API_KEY')
+const apiKey = process.env.GEMINI_API_KEY || ''
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
+const genAI = new GoogleGenerativeAI(apiKey)
+
 export default genAI
